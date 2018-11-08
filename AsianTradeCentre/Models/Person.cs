7c427bf181a31e678537a9d4cb8a1263553185ec
David Thompson <dthompson1411@gmail.com>
2018-11-08 12:31:33 +0800

    Initial Program push to GitHub

---
+++ AsianTradeCentre/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsianTradeCentre.Models
+{
+    public class Person
+    {
+
+
+
+
+    }
+}
