b57bfc4ec8bd399eeb3bcb36f23c545c55fe3936
David Thompson <dthompson1411@gmail.com>
2018-11-08 12:35:36 +0800

    Learning Push Pull method of Git

--- AsianTradeCentre/Controllers/HomeController.cs
+++ AsianTradeCentre/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace AsianTradeCentre.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your  description page.";
 
             return View();
         }
